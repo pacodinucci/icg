52c35d73edfa4e2c9eaec6d21a2aad09619410ae
randeep singh <springsoft008@gmail.com>
2022-06-29 10:18:35 +0100

    next question disabled

--- MyCvTracker/src/interviewApp.js
+++ MyCvTracker/src/interviewApp.js
@@ -25,6 +25,7 @@ function startRecording() {
 
 	var recordButton = document.getElementById("recordButton");
     var stopButton = document.getElementById("stopButton");
+    var nextQuestion = document.getElementById("nextQuestion");
    // var pauseButton = document.getElementById("pauseButton");
 
 	/*
@@ -41,6 +42,7 @@ function startRecording() {
     recordButton.innerHTML = "Recording...";
 	recordButton.disabled = true;
 	stopButton.disabled = false;
+	nextQuestion.disabled = true;
 	//pauseButton.disabled = false
 
 	/*
@@ -109,12 +111,13 @@ function stopRecording() {
     //var pauseButton = document.getElementById("pauseButton");
     document.getElementById("endTime").value = new Date();
     recordButton.innerHTML = "Record";
-
+    var nextQuestion = document.getElementById("nextQuestion");
 
 	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
 	//pauseButton.disabled = true;
+	nextQuestion.disabled = false;
 
 	//reset button just in case the recording is stopped while paused
 	//pauseButton.innerHTML="Pause";
