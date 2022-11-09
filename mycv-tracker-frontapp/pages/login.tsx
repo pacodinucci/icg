d85c3edae15d163bc39e30c77a209ffbcca58e90
Tushar Agrawal <tusharsagrawal16@gmail.com>
2022-11-09 14:02:28 +0530

    login and register design complete

--- mycv-tracker-frontapp/pages/login.tsx
+++ mycv-tracker-frontapp/pages/login.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
-import { Col, Container, Card, CardTitle, CardBody, Form, FormGroup, Label, Input } from "reactstrap";
+import { Col, Row, Container, Card, CardTitle, CardBody, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import Link from "next/link";
+
+import styles from "../styles/Login.module.css";
 
 const Login: NextPage = () => {
   const [details, setDetails] = useState({ email: "", password: "" });
@@ -11,13 +13,17 @@ const Login: NextPage = () => {
   };
 
   return (
-    <Container>
-      <Col md={6} className="my-5">
-        <Card body className="text-center">
-          <CardTitle tag="h2">Sign In to MyCVTracker</CardTitle>
+    <Container className="p-5">
+      <Col md={7} className="my-5">
+        <Card body className={`text-center ${styles.box} fs-6`}>
+          <CardTitle tag="h2" className="fs-1">
+            Sign In to MyCVTracker
+          </CardTitle>
           <CardBody>
-            <Link href="/register">New to MyCVTracker? Sign up!</Link>
-            <Form className="my-4">
+            <Link href="/register" className="text-decoration-none fs-4">
+              New to MyCVTracker? Sign up!
+            </Link>
+            <Form className="my-4 text-end">
               <FormGroup row>
                 <Label for="email" sm={2}>
                   Email
@@ -46,6 +52,22 @@ const Login: NextPage = () => {
                     value={details.password}
                     onChange={handleChangeInput}
                   />
+                </Col>
+              </FormGroup>
+              <FormGroup check row className="my-4">
+                <Col sm={{ offset: 2, size: 10 }}>
+                  <Input type="checkbox" id="rememberMe" name="rememberMe" />
+                  <Label for="rememberMe" className=" float-start" check>
+                    Remember Me
+                  </Label>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm={{ offset: 2, size: 10 }} className="justify-content-between d-flex">
+                  <Button outline>Sign In</Button>
+                  <Button outline color="link">
+                    Forgot password?
+                  </Button>
                 </Col>
               </FormGroup>
             </Form>
--- mycv-tracker-frontapp/pages/register.tsx
+++ mycv-tracker-frontapp/pages/register.tsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
-import { Col, Container, Card, CardTitle, CardBody, Form, FormGroup, Label, Input } from "reactstrap";
+import { Col, Container, Card, CardTitle, CardBody, Form, FormGroup, Label, Input, Button, Row } from "reactstrap";
 import Link from "next/link";
 
+import styles from "../styles/Register.module.css";
+
 const Register: NextPage = () => {
-  const [details, setDetails] = useState({ email: "", password: "", firstName: "", lastName: "" });
+  const [details, setDetails] = useState({ email: "", password: "", firstName: "", lastName: "", confirmPassword: "" });
 
   const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDetails((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
   return (
-    <Container>
-      <Col md={6} className="my-5">
-        <Card body className="text-center">
-          <CardTitle tag="h2">Sign Up to MyCVTracker</CardTitle>
+    <Container className="p-5">
+      <Col md={7} className="my-5">
+        <Card body className={`text-center ${styles.box} fs-6`}>
+          <CardTitle tag="h2" className="fs-1">
+            Sign Up to MyCVTracker
+          </CardTitle>
           <CardBody>
-            <Link href="/login">Already have a MyCVTracker account? Sign in!</Link>
-            <Form className="my-4">
+            <Link href="/login" className="text-decoration-none fs-4">
+              Already have a MyCVTracker account? Sign in!
+            </Link>
+            <Form className="my-4 text-end">
               <FormGroup row>
                 <Label for="firstName" sm={2}>
                   First Name
@@ -80,6 +86,40 @@ const Register: NextPage = () => {
                   />
                 </Col>
               </FormGroup>
+              <FormGroup row>
+                <Label for="confirmPassword" sm={2}>
+                  password
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    placeholder="confirm password"
+                    type="password"
+                    value={details.confirmPassword}
+                    onChange={handleChangeInput}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm={{ offset: 2, size: 10 }} className="justify-content-between d-flex">
+                  <Button outline>Sign Up</Button>
+                </Col>
+              </FormGroup>
+              <Row>
+                <Col sm={{ offset: 2, size: 10 }} className="text-start">
+                  <p>
+                    By Signing up you agree to our{" "}
+                    <Link href="/terms" className="text-decoration-none">
+                      Terms & Conditions
+                    </Link>
+                  </p>
+                  <p>
+                    After form submission, you will receive an email from info@mycvtracker.com to activate your account.
+                    Please check your spam folder if it&apos;s not in your inbox in next few minutes.
+                  </p>
+                </Col>
+              </Row>
             </Form>
           </CardBody>
         </Card>
