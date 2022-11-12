ed3d211c564a40a5f2dcd37e64b3bd1e5005b1cd
Tushar Agrawal <tusharsagrawal16@gmail.com>
2022-11-12 13:03:31 +0530

    Pages Update
    add terms page
    
    initialized account pages

---
+++ mycv-tracker-frontapp/pages/terms.tsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+import { Container, Row } from "reactstrap";
+
+const Terms = () => {
+  return (
+    <Container className="p-3">
+      <Row className="my-2">
+        <h2 className="fs-1 fw-bold">Terms and Conditions (&quot;Terms&quot;) </h2>
+      </Row>
+      <Row className="my-3">
+        <p>Last updated: 25 April 2019</p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <p>
+          Welcome to My CV Tracker. Please read these Terms and Conditions (&quot;Terms&quot;, &quot;Terms and
+          Conditions&quot;) carefully before accessing, using, sharing or obtaining any materials, information, products
+          or services. Your access to and use of the mycvtracker.com website (the &quot;Service&quot;) operated by My CV
+          Tracker (&quot;us&quot;, &quot;we&quot; or &quot;our&quot;) is subject to your acceptance of and compliance
+          with these terms. These Terms apply to all visitors, users and others who access and/or use the Service.
+        </p>
+        <p>
+          By accessing or using the Service, you agree to be bound by these Terms. If you disagree with any or part of
+          these Terms then you may not access or use the Service.
+        </p>
+        <p>
+          We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is
+          material we will try to provide at least 30 days notice prior to any new terms taking effect through our
+          Services, or by other means, to provide you the opportunity to review the changes before they come into
+          effect. What constitutes a material change will be determined at our sole discretion. Your continued use of
+          our Services after we publish or send a notice about our changes to these terms means that you are consenting
+          to the updated terms. If you object to any changes, you should immediately terminate use of our website and
+          close your account.
+        </p>
+        <p>All content on this site is the property of My CV Tracker. Unauthorised copying is not permitted.</p>
+        <p>My CV Tracker goes to great effort to ensure the information on its website is accurate.</p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Order with My CV Tracker</strong>
+        <p>
+          In order to complete your order we collect personal information such as contact details, education and
+          employment history.
+        </p>
+        <p>If you wish to pay by other means, please make contact to discuss alternative methods.</p>
+        <p>
+          In order to be as efficient as possible we do recommend that you outline any requirements in an email and send
+          this to
+          <a href="mailto:info@mycvtracker.com">
+            <strong>info@mycvtracker.com</strong>
+          </a>
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Information on Site:</strong>
+        <p>
+          Whilst we will try to make every effort to ensure that the information on this site is accurate and complete,
+          some of the information is supplied to us by third parties and we are not physically able to check the
+          accuracy or completeness of such information. We do not accept any liability arising from any inaccuracy or
+          omission in any of the information on this site or any liability in respect of information on this site
+          supplied by you, any other site user or any other person.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Use of Our Website and Service:</strong>
+        <p>
+          You may only use this website for lawful purposes when seeking employment or help with your career. You are
+          solely responsible for any information submitted by you to this website. You are responsible for ensuring that
+          all information supplied by you is true, accurate, up-to-date and not likely to mislead or deceive and that it
+          is not discriminatory, obscene, offensive, defamatory or otherwise illegal, unlawful or in breach of any
+          applicable legislation, regulations, guidelines or codes of practice or the copyright, trade mark or other
+          intellectual property rights of any person in any jurisdiction.
+        </p>
+        <p>
+          If any of your information changes, you must immediately update your account details to reflect those changes.
+        </p>
+        <p>
+          We reserve the right to remove any information supplied by you from this website at our sole discretion, at
+          any time and for any reason without being required to give any explanation.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Our Service</strong>
+        <p>
+          Once you upload your CV we will distribute your CV to the UK&apos;s leading job boards, recruiters and our
+          network. The aim is to match your specific skillset with relevant contracts.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Your Account</strong>
+        <p>By registering for an account, you agree to:</p>
+        <p>1. Create a strong and secure password;</p>
+        <p>2. Keep your password secure and confidential;</p>
+        <p>
+          3. Promptly change your Personal information should you believe that your account is no longer secure (e.g.
+          loss, theft or unauthorised disclosure or use of your information or computer or mobile device used to access
+          Our Website).
+        </p>
+        <p>You are responsible for anything that happens through your account unless you close it or report misuse.</p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Protection and Use of Information</strong>
+        <p>
+          The Internet is not a secure medium. However, My CV Tracker will use its reasonable endeavors to keep the
+          personal information it receives about you confidential, although nothing in these terms and conditions shall
+          restrict or prohibit My CV Tracker disclosing personal information to recruitment agencies or Job Boards in
+          circumstances where you have expressly or by implication given your consent to do so.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Security and Passwords</strong>
+        <p>
+          In order to register with this site and to sign in when you visit the site, you will need to use a user name
+          and password. You are solely responsible for the security and proper use of your password, which should be
+          kept confidential at all times and not disclosed to any other person. You must notify us immediately if you
+          believe that your password is known to someone else or if it may be used in an unauthorised way. We accept no
+          liability for any unauthorised or improper use or disclosure of any password.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Termination</strong>
+        <p>
+          We may terminate your registration and/or deny you access to the site or any part of it (including any
+          services, goods or information available on or through this site) at any time in our absolute discretion and
+          without any explanation or notification if we feel that you are using this site in an improper or unlawful
+          way.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Refunds</strong>
+        <p>No refunds will be offered.</p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Delivery &amp; Distribution</strong>
+        <p>
+          Once the payment has been made for these services, CV distribution takes place immediately as per the selected
+          sectors and job titles. As soon as this process is completed the service will be considered fulfilled.
+        </p>
+        <p>
+          In case you have uploaded a blank document or wish to update any information in your CV at the time of signing
+          up for this service, please contact us via support at
+          <a href="mailto:info@mycvtracker.com">info@mycvtracker.com</a>
+          within 24 hours with the correct details.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Liability</strong>
+        <p>
+          We accept no liability for any loss (whether direct or indirect, for loss of business, revenue or profits,
+          wasted expenditure, corruption or destruction of data or for any other indirect or consequential loss
+          whatsoever) arising from your use of this website and we hereby exclude any such liability, whether in
+          contract, tort (including for negligence) or otherwise. We hereby exclude all representations, warranties and
+          conditions relating to this website and your use of it to the maximum extent permitted by law.
+        </p>
+        <p>
+          The information contained in this site may contain technical inaccuracies and typographical errors. All
+          information made available from within this website is provided without warranty of any kind, either expresses
+          or implied on the part of My CV Tracker.
+        </p>
+        <p>
+          My CV Tracker excludes all liability it may have to you to the fullest extent permitted by law (and whether
+          such liability arises in contract, tort or otherwise) as a result of your use of this website or your
+          accepting of a job vacancy obtained directly or indirectly through the website or following use of the website
+          or any of our associated services.
+        </p>
+        <p>
+          Nothing in these terms and conditions shall exclude My CV Tracker&apos;s liability for death or personal
+          injury due to its negligence or for fraudulent misrepresentation.
+        </p>
+        <p>
+          You agree to indemnify us and keep us indemnified against all costs, expenses, claims, losses, liabilities or
+          proceedings arising from the use or misuse by you of this site or of any of the associated services you
+          receive or have use of.
+        </p>
+        <p>
+          You agree to defend, indemnify, and hold harmless the Company, its officers, directors, employees and agents,
+          from and against any claims, actions or demands, including without limitation reasonable legal and accounting
+          fees, alleging or resulting from your use of the Material or your breach of the terms of these Terms and
+          Conditions.
+        </p>
+      </Row>
+      <Row tag="section" className="my-3">
+        <strong>Indemnity</strong>
+        <p>
+          You agree to defend, indemnify and hold harmless My CV Tracker and its employees, officers, directors,
+          shareholders, affiliates, agents, representatives, suppliers and members, from and against all claims, losses,
+          costs and expenses (including attorneys fees) arising out of (a) your use of, or activities in connection
+          with, the Site (including all Transactions); or (b) any violation of this Agreement by you.
+        </p>
+      </Row>
+    </Container>
+  );
+};
+
+export default Terms;
