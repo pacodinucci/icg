488e9e2e2dbacac8eab04285a3ec1a266118a794
Tushar Agrawal <tusharsagrawal16@gmail.com>
2022-11-24 15:24:04 +0530

    Interview Response basic interface working using howlerjs

--- MyCvTracker/src/interviewResponse.js
+++ MyCvTracker/src/interviewResponse.js
@@ -1,5 +1,4 @@
 var TOKEN_REF = "token";
-
 window.addEventListener("DOMContentLoaded", function (event) {
   console.log(window.location.search);
   console.log("DOM fully loaded and parsed");
@@ -57,10 +56,10 @@ function fetchResponses(token) {
 
 function displayResponses(responses, token) {
   var section = document.getElementById("responses");
-  var candidate = document.createElement("div");
-  candidate.classList.add("row", "alert", "alert-info");
-  candidate.innerText = "Candidate Email: " + responses[0].candidate;
-  section.appendChild(candidate);
+  // var candidate = document.createElement("div");
+  // candidate.classList.add("row", "alert", "alert-info");
+  // candidate.innerText = "Candidate Email: " + responses[0].candidate;
+  // section.appendChild(candidate);
   responses.forEach(function (response, index) {
     var res = createResponseElement(response, index + 1, token);
     section.appendChild(res);
@@ -77,6 +76,12 @@ function createResponseElement(response, qNumber, token) {
   //   questionId: number;
   // }
 
+  var sound = new Howl({
+    src: ["https://mycvtracker.com:8080/interviews/audioData/" + token + "/" + response.questionId],
+    // html5: true,
+    format: ["wav"],
+  });
+
   var item = document.createElement("div");
   item.style.marginTop = 20 + "px";
   var row1 = document.createElement("div");
@@ -87,26 +92,66 @@ function createResponseElement(response, qNumber, token) {
   row1.classList.add("row");
   row1.appendChild(quesId);
   row1.appendChild(ques);
-
   item.appendChild(row1);
 
   var row2 = document.createElement("div");
   row2.classList.add("row");
-  var audio = document.createElement("audio");
-  audio.controls = "controls";
-  audio.src = "https://mycvtracker.com:8080/interviews/audioData/" + token + "/" + response.questionId;
-  audio.style.width = "100%";
-  audio.style.borderRadius = "10px";
-  audio.type = "audio/wav";
 
-  var label = document.createElement("p");
-  label.innerText = "Audio Response: ";
-  row2.appendChild(label);
-  row2.appendChild(audio);
-  item.appendChild(row2);
+  row2.style.marginTop = "10px";
+
+  var startBtn = document.createElement("button");
+  var playing = false;
+  startBtn.addEventListener("click", function () {
+    playing = !playing;
+    if (playing) {
+      startBtn.innerText = "Pause";
+      sound.play();
+    } else {
+      startBtn.innerText = "Resume";
+      sound.pause();
+    }
+  });
+
+  var loadBtn = document.createElement("button");
+  loadBtn.classList.add("btn", "btn-info", "disabled");
+  loadBtn.innerText = "Loading";
+
+  startBtn.classList.add("btn", "btn-success", "disabled");
+  startBtn.innerText = "Play";
+  var stopBtn = document.createElement("button");
+  stopBtn.style.marginLeft = "10px";
+  loadBtn.style.marginRight = "10px";
+  stopBtn.addEventListener("click", function () {
+    startBtn.innerText = "Play";
+    sound.stop();
+  });
+  stopBtn.classList.add("btn", "btn-danger", "disabled");
+  stopBtn.innerText = "Stop";
+  row2.appendChild(loadBtn);
+  row2.appendChild(startBtn);
+  row2.appendChild(stopBtn);
+
+  sound.on("load", function () {
+    console.log("loaded");
+    loadBtn.remove();
+    stopBtn.classList.remove("disabled");
+    startBtn.classList.remove("disabled");
+  });
+
+  loadBtn.addEventListener("click", function () {
+    sound.load();
+    loadBtn.classList.add("disabled");
+  });
+
+  sound.on("loaderror", function (e) {
+    loadBtn.innerText = "Error during Load, Click to Retry";
+    loadBtn.classList.remove("disabled");
+    console.log(e);
+  });
 
   var hr = document.createElement("hr");
-  item.appendChild(hr);
 
+  item.appendChild(row2);
+  item.appendChild(hr);
   return item;
 }
