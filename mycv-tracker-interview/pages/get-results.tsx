b9a6aea0f5645d182ae8f92fb596e79762821d2d
Tushar Agrawal <tusharsagrawal16@gmail.com>
2022-12-02 13:52:58 +0530

    migrate get-results to mantine

--- mycv-tracker-interview/pages/get-results.tsx
+++ mycv-tracker-interview/pages/get-results.tsx
@@ -1,25 +1,14 @@
 import Link from "next/link";
 import React, { useState, useCallback } from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Container,
-  Row,
-  Card,
-  CardBody,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button,
-  FormFeedback,
-  Col,
-} from "reactstrap";
+
+import { Container, Button, TextInput, Paper, Title } from "@mantine/core";
+
 import { sendGetCandidateResult } from "../apis/mycvtracker";
 import { useToast } from "../hooks/useToast";
 import { useUserState } from "../hooks/useUserState";
 import { CandidateResultRequest } from "../types/assignInterview_types";
 import { alerts } from "../utils/alert-utils";
+import { useForm } from "@mantine/form";
 
 type InputValuesError = {
   candidateName: string;
@@ -29,51 +18,30 @@ type InputValuesError = {
 };
 
 const GetResults = () => {
+  const details = useForm({
+    initialValues: {
+      candidate: "",
+      candidateEmail: "",
+      candidateList: "",
+      candidateName: "",
+      interviewType: "",
+      invite: "",
+      jobLink: "",
+      resultOwners: "",
+      token: "",
+    },
+    validate: {
+      candidate: (value) => (value.length < 5 ? "Invalid Candidate Email" : null),
+      token: (value) => (value.length < 5 ? "Invalid Token" : null),
+    },
+  });
+
   const { token } = useUserState();
   const { showErrorToast, showSuccessToast } = useToast();
   const [isLoading, setIsLoading] = useState(false);
-  const [values, setValues] = useState<CandidateResultRequest>({
-    candidate: "",
-    candidateEmail: "",
-    candidateList: "",
-    candidateName: "",
-    interviewType: "",
-    invite: "",
-    jobLink: "",
-    resultOwners: "",
-    token: "",
-  });
-
-  const [errors, setErrors] = useState<InputValuesError>({
-    candidateName: "",
-    candidate: "",
-    token: "",
-    jobLink: "",
-  });
-
-  const handleChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
-  }, []);
 
   const handleFormSubmit = useCallback(
-    async (values: CandidateResultRequest, e?: React.FormEvent<HTMLFormElement>) => {
-      if (e) e.preventDefault();
-      let hasError = false;
-      if (values.candidate.length <= 5) {
-        setErrors((prev) => ({ ...prev, candidate: "Invalid Candidate Email" }));
-        hasError = true;
-      }
-      //   if (values.candidateName.length <= 0) {
-      //     setErrors((prev) => ({ ...prev, candidateName: "Candidate Name cannot be empty" }));
-      //     hasError = true;
-      //   }
-
-      if (values.token.length <= 4) {
-        setErrors((prev) => ({ ...prev, token: "Invalid Token" }));
-        hasError = true;
-      }
-
-      if (hasError) return;
+    async (values: CandidateResultRequest) => {
       try {
         setIsLoading(true);
         await sendGetCandidateResult(values, token);
@@ -90,81 +58,25 @@ const GetResults = () => {
   );
 
   return (
-    <Container className="py-5">
-      <Row>
-        <p className="fs-1 my-3">Get Interview Results</p>
-      </Row>
-      <Row className="fs-4">
-        <Col>
-          <Card>
-            <CardBody>
-              <Breadcrumb>
-                <BreadcrumbItem>
-                  <Link href="/dashboard">Dashboard</Link>
-                </BreadcrumbItem>
-                <BreadcrumbItem active>Get Results</BreadcrumbItem>
-              </Breadcrumb>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+    <Container>
+      <Title order={1}>Get Interview Results</Title>
 
-      <Form onSubmit={(e) => handleFormSubmit(values, e)}>
-        <FormGroup className="my-3">
-          <Label for="candidate">Candidate Email</Label>
-          <Input
-            name="candidate"
-            id="candidate"
-            type="email"
-            onChange={handleChangeInput}
-            value={values.candidate}
-            invalid={errors.candidate.length > 2}
+      <Paper p="md" my="md">
+        <form onSubmit={details.onSubmit(handleFormSubmit)}>
+          <TextInput
+            placeholder="johndoe@email.com"
+            label="Candidate Email"
+            withAsterisk
+            {...details.getInputProps("candidate")}
           />
-          <FormFeedback>{errors.candidate}</FormFeedback>
-        </FormGroup>
-        <FormGroup className="my-3">
-          <Label for="token">Token</Label>
-          <Input
-            name="token"
-            id="token"
-            type="text"
-            onChange={handleChangeInput}
-            value={values.token}
-            invalid={errors.token.length > 2}
-          />
-          <FormFeedback>{errors.token}</FormFeedback>
-        </FormGroup>
-
-        <FormGroup className="my-3">
-          <Label for="candidateName">Candidate Name</Label>
-          <Input
-            name="candidateName"
-            id="candidateName"
-            type="text"
-            onChange={handleChangeInput}
-            value={values.candidateName}
-            invalid={errors.candidateName.length > 2}
-          />
-          <FormFeedback>{errors.candidateName}</FormFeedback>
-        </FormGroup>
-
-        <FormGroup className="my-3">
-          <Label for="jobLink">job Link</Label>
-          <Input
-            name="jobLink"
-            id="jobLink"
-            type="text"
-            onChange={handleChangeInput}
-            value={values.jobLink}
-            invalid={errors.jobLink.length > 2}
-          />
-          <FormFeedback>{errors.jobLink}</FormFeedback>
-        </FormGroup>
-
-        <Button type="submit" color="primary" disabled={isLoading}>
-          Get Interview Results
-        </Button>
-      </Form>
+          <TextInput placeholder="token" label="Token" {...details.getInputProps("token")} withAsterisk />
+          <TextInput placeholder="John Doe" label="Candidate Name" {...details.getInputProps("candidateName")} />
+          <TextInput placeholder="https://mycvtracker.com" label="Job Link" {...details.getInputProps("lobLink")} />
+          <Button type="submit" disabled={isLoading} mt="lg">
+            Get Interview Results
+          </Button>
+        </form>
+      </Paper>
     </Container>
   );
 };
